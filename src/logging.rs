2f8549d9d97851470552eceb023093219c76893e
Nathan Richman <naterichman@flywheel.io>
2022-03-16 19:58:29 -0500

    ENH: Add logging, mostly for debugging actions.
    I could and do use gdb, but it's also really nice to see the actions in
    real time without breakpoints.

---
+++ src/logging.rs
@@ -0,0 +1,21 @@
+use log::{LevelFilter, SetLoggerError};
+use log4rs::append::file::FileAppender;
+use log4rs::encode::pattern::PatternEncoder;
+use log4rs::config::{Appender, Config, Root};
+
+pub fn setup_log() -> Result<(), SetLoggerError> {
+    let mut log_path = dirs::home_dir().unwrap();
+    log_path.push(".rgm.log");
+    let logfile = FileAppender::builder()
+        .encoder(Box::new(PatternEncoder::new("{l} - {m}\n")))
+        .build(log_path).unwrap();
+
+    let config = Config::builder()
+        .appender(Appender::builder().build("logfile", Box::new(logfile)))
+        .build(Root::builder()
+                   .appender("logfile")
+                   .build(LevelFilter::Info)).unwrap();
+
+    log4rs::init_config(config)?;
+    Ok(())
+}
