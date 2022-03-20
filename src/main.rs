bc614067a6d420f971da89ac598f5f22903066b9
Nathan Richman <naterichman@flywheel.io>
2022-03-19 20:25:13 -0500

    MAIN: Start copying in list example from tui

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,23 @@
 use args::{Cli, Commands};
 use clap::Parser;
 use repo::Repos;
-use std::process;
-use std::io::stdout;
-use screen::Screen;
+use std::{
+    process,
+    io,
+    error::Error,
+    time::{Duration, Instant}
+};
 use logging::setup_log;
+use tui::{
+    backend::{Backend, CrosstermBackend},
+    Terminal,
+    widgets::{Block, Borders, List, ListItem, ListState},
+};
+use crossterm::{
+    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+};
 
 mod args;
 mod repo;
@@ -62,17 +75,7 @@ fn main() {
         None => {
             let repos = Repos::load();
             match repos {
-                Ok(r) => {
-                    let mut screen = Screen::new(r, stdout());
-                    if let Err(e) = screen.start() {
-                        println!("{}", e);
-                    }
-                    loop {
-                        if !screen.update() {
-                            break
-                        }
-                    }
-                }
+                Ok(r) => run_interactive(repos),
                 Err(e) => println!("{:?}", e)
             }
             //match repos {
@@ -85,4 +88,128 @@ fn main() {
            // }
         }
     }
+}
+
+struct StatefulList<T> {
+    state: ListState,
+    items: Vec<T>,
+}
+
+impl<T> StatefulList<T> {
+    fn with_items(items: Vec<T>) -> StatefulList<T> {
+        StatefulList {
+            state: ListState::default(),
+            items,
+        }
+    }
+
+    fn next(&mut self) {
+        let i = match self.state.selected() {
+            Some(i) => {
+                if i >= self.items.len() - 1 {
+                    0
+                } else {
+                    i + 1
+                }
+            }
+            None => 0,
+        };
+        self.state.select(Some(i));
+    }
+
+    fn previous(&mut self) {
+        let i = match self.state.selected() {
+            Some(i) => {
+                if i == 0 {
+                    self.items.len() - 1
+                } else {
+                    i - 1
+                }
+            }
+            None => 0,
+        };
+        self.state.select(Some(i));
+    }
+}
+
+struct App<'a> {
+    items: StatefulList<Repo>,
+}
+
+pub fn run_interactive(repos: Repos) -> Result<(), Box<dyn Error>>{
+    enable_raw_mode()?;
+    let mut stdout = io::stdout();
+    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+
+    // create app and run it
+    let tick_rate = Duration::from_millis(250);
+    loop {
+        terminal.draw(|f|{
+            let size = f.size();
+            let items: Vec<ListItem> = app
+                .items
+                .items
+                .iter()
+                .map(|i| {
+                    // TODO: RepoView::get_text
+                    let mut lines = vec![Spans::from(i.0)];
+                    for _ in 0..i.1 {
+                        lines.push(Spans::from(Span::styled(
+                            "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+                            Style::default().add_modifier(Modifier::ITALIC),
+                        )));
+                    }
+                    ListItem::new(lines).style(Style::default().fg(Color::Black).bg(Color::White))
+                })
+                .collect();
+
+            // Create a List from all list items and highlight the currently selected one
+            let items = List::new(items)
+                .block(Block::default().borders(Borders::ALL).title("List"))
+                .highlight_style(
+                    Style::default()
+                        .bg(Color::LightGreen)
+                        .add_modifier(Modifier::BOLD),
+                )
+                .highlight_symbol(">> ");
+
+            // We can now render the item list
+            f.render_stateful_widget(items, size, &mut app.items.state);
+        })?;
+
+        let timeout = tick_rate
+            .checked_sub(last_tick.elapsed())
+            .unwrap_or_else(|| Duration::from_secs(0));
+        if crossterm::event::poll(timeout)? {
+            if let Event::Key(key) = event::read()? {
+                match key.code {
+                    KeyCode::Char('q') => return Ok(()),
+                    KeyCode::Left => app.items.unselect(),
+                    KeyCode::Down => app.items.next(),
+                    KeyCode::Up => app.items.previous(),
+                    _ => {}
+                }
+            }
+        }
+        if last_tick.elapsed() >= tick_rate {
+            app.on_tick();
+            last_tick = Instant::now();
+        }
+    }
+    }
+    disable_raw_mode()?;
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+    )?;
+    terminal.show_cursor()?;
+
+    if let Err(err) = res {
+        println!("{:?}", err)
+    }
+
+    Ok(())
 }
